492ab8c7702f1844baf3daf2e90436e9a7b4791c
Saehwa95 <tpghk1002@naver.com>
2022-12-24 09:34:11 +0900

    :art: 글로벌 스타일 추가

--- src/components/ui/GlobalStyle.jsx
+++ src/components/ui/GlobalStyle.jsx
@@ -3,6 +3,9 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 
   body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
     width: 100%;
     background-color: #121212;
     color: whitesmoke;
